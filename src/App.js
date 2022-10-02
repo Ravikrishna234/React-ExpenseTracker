11b01d63395fb081381a0f3af0ce11b7cf9fbc69
RaviKrishna <ravikrishnadontula23@msitprogram.net>
2022-10-02 00:58:08 +0000

    Victorycommit-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./styles.css";
-import ExpenseItem from "./components/ExpenseItem";
-import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 export default function App() {
+  const addExpenseHandler = (expense) => {};
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expenses></Expenses>
     </div>
   );
