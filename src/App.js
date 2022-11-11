db8587ebc4bccd1866754c34cbbf4dfef66a2758
Ravikrishna234 <41903289+Ravikrishna234@users.noreply.github.com>
2022-11-11 16:05:56 -0600

    Victory

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import NewExpense from "./components/NewExpense/NewExpense";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
